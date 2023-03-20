bb7696d17c19f17342e22fb719fba9f4d0635fab
alfandi <fandidixpc1@gmail.com>
2023-03-20 14:50:36 +0700

    Add project files.

---
+++ Latihan4_152/Latihan4_152.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+float LuasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+int main()
+{
+	float panjang, lebar, jejari, alas, tinggi:
+	int pilihan;
+
+	do {
+		cout << "\n\n================" << endl;
+		cout << "=======MENU========" << endl;
+		cout << "===================" << endl;
+		cout "1. Luas Persegi" << endl;
+		cout "2. Luas Lingkaran" << endl;
+		cout "3. Luas Persegi" << endl;
+	}
+}
